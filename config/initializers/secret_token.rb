d95836753d41ab0ce9755e3bea21cb3bfab4ab87
Lee Hong Seok   k <hongs@snuev.com>
2013-04-17 19:55:01 +0900

    initium

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Profile::Application.config.secret_token = 'fd1adec173ef27950ac8d4ef1e028fedea78b1cba5d99c07532bac53c245b6eca0720cdb9d0d76185ccc9ff9d0031e60cee072ed02ea61e4c3282f66b560c2af'
