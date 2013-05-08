f75abceed536f18b516677d8c085ff1645354553
hongs <jang2poom@naver.com>
2013-05-08 11:05:33 +0900

    no_mail

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,20 +1,5 @@
 class WelcomeController < ApplicationController
 def index2
-
-  require 'gmail'
-  def sendmail_to email_address
-      gmail = Gmail.connect("jang2poom@gmail.com", "sinclair90")
-      gmail.deliver do
-        to email_address 
-        subject "there was an access on hongsworld"
-        text_part do
-          body "there was an access on hongsworld"
-  
-        end
-     end
-     gmail.logout
-  end
-  sendmail_to "jang2poom@naver.com"
 end
 def test1
 end
